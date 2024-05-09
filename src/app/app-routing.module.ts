cf81fab731b9b3f0d64a00fcfb6335abd9feed70
Oleksii <oleksijkolotilo63@gmail.com>
2024-05-09 19:07:08 +0300

    added few routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {LoginComponent} from "./login/login.component";
 import {RegistrationComponent} from "./registration/registration.component";
 import {PostListComponent} from "./post-list/post-list.component";
 import {PostCommentsComponent} from "./post-comments/post-comments.component";
+import {ChatListComponent} from "./chat-list/chat-list.component";
 
 const routes: Routes = [
   { path: "", component: PostListComponent },
   { path: "login", component: LoginComponent },
   { path: "sign-up", component: RegistrationComponent },
-  { path: "comments/:postId", component: PostCommentsComponent }
+  { path: "comments/:postId", component: PostCommentsComponent },
+  { path: "messages", component: ChatListComponent }
 ];
 
 @NgModule({
